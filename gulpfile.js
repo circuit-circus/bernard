2d329d944bf8371341cbc0dbf87d130f02abe662
Jesper Fogh <foghjesperjhf@gmail.com>
2017-01-02 18:25:32 +0100

    Removed the notify because it annoyed the crap out of me.

--- gulpfile.js
+++ gulpfile.js
@@ -17,13 +17,13 @@ gulp.task('styles', function() {
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(minifycss({compatibility: 'ie9'}))
     .pipe(gulp.dest('src/'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(livereload(server));
 });
 
 gulp.task('watch', function() {
   livereload.listen();
-  // Watch .scss files
+  // Watch .less files
+  gulp.start('styles');
   gulp.watch('src/less/**/*.less', ['styles']);
 });
 
